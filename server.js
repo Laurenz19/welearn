63aa0d18e3354a16a44b2b13a284ebe81321a928
laurenz19 <laurenziosambany@gmail.com>
2022-10-24 18:30:32 +0300

    initial commit

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const path = require('path')
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.get(/.*/, (req, res)=>{
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
 
-const port = process.env || 7070
+
+const port = 8080
 app.listen(port)
 
 console.log(`app listen on port ${port}`)
