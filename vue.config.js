373e3bb8fe974cf75f672c9953575ccfc312665a
laurenz19 <laurenziosambany@gmail.com>
2022-10-24 19:44:49 +0300

    add vue config

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    chainWebpack: config => {
+      config.module
+        .rule('images')
+          .set('parser', {
+            dataUrlCondition: {
+              maxSize: 4 * 1024 // 4KiB
+            }
+          })
+    }
+  }
